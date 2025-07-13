98bdb3ae2d941e0ccf4060becad8b76d16cc7d64
ryo <rhtky03041993@gmail.com>
2025-07-13 17:55:31 +0900

    日付入力に初期値を設定

--- forms.py
+++ forms.py
@@ -1,5 +1,6 @@
 from django import forms
+import datetime
 
 class DateRangeForm(forms.Form):
-    start_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
-    end_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    start_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}),initial=datetime.date(2024, 1, 1))
+    end_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}),initial=datetime.date(2024, 1, 1))
